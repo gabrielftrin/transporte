877a946e3885cc381bf482ac1bb4e442e6ad1e7a
Gabriel Fernandes Trindade <mrgabrielfernandes@hotmail.com>
2020-03-10 17:08:39 -0300

    atualizacoes

---
+++ GestaoDeRecargas/telainicial.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace GestaoDeRecargas
+{
+    partial class GestaoDeRecargas
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GestaoDeRecargas));
+            this.Cartoes = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Nome = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Saldo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Cartoes
+            // 
+            this.Cartoes.Location = new System.Drawing.Point(12, 12);
+            this.Cartoes.Name = "Cartoes";
+            this.Cartoes.Size = new System.Drawing.Size(183, 75);
+            this.Cartoes.TabIndex = 0;
+            this.Cartoes.Text = "Gerenciar cartões de passagem";
+            this.Cartoes.UseVisualStyleBackColor = true;
+            this.Cartoes.Click += new System.EventHandler(this.GerenciarCartoes);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nome,
+            this.Saldo});
+            this.dataGridView1.Location = new System.Drawing.Point(235, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(513, 368);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Nome
+            // 
+            this.Nome.HeaderText = "Nome do cabeçalho";
+            this.Nome.Name = "Nome";
+            // 
+            // Saldo
+            // 
+            this.Saldo.HeaderText = "Saldo";
+            this.Saldo.Name = "Saldo";
+            // 
+            // GestaoDeRecargas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 433);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Cartoes);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "GestaoDeRecargas";
+            this.Text = "Gestão de Recargas";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Cartoes;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewButtonColumn Nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Saldo;
+    }
+}
+
--- GestaoDeRecargas/telainicial.cs
+++ GestaoDeRecargas/telainicial.cs
@@ -16,9 +16,22 @@ namespace GestaoDeRecargas
         {
             InitializeComponent();
         }
-                private void Button1_Click(object sender, EventArgs e)
+        private void GerenciarCartoes(object sender, EventArgs e)
         {
             MessageBox.Show("Mensagem box... caixa de mensagem");
+            //TelaCartoes tela = new TelaCartoes();
+            //tela.Click += new System.EventHandler(this.TelaCartoes;
+            //this.Cartoes.Click += new System.EventHandler(this.Cartoes);
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GroupBox1_Enter(object sender, EventArgs e)
+        {
+
         }
     }
 }
